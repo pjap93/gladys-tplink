8105fc2cfc639fbb126b0bc4011be3124ffe5cac
pjap93 <pjap9392@gmail.com>
2017-07-22 18:01:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = function(sails) {
     
     var setup = require('./lib/setup.js');
+    var exec = require('./lib/exec.js');
     
     return {
-        setup: setup
+        setup: setup,
+	    exec: exec
         };
 };
