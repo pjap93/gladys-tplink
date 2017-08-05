c9b98afbdc253f1247caec3121a9e02699069de1
pjap93 <pjap9392@gmail.com>
2017-08-05 10:19:49 +0200

    update
    - Créaton d'une fonction send pour l'envoye groupé des paramètres vers les lampes depuis un script
    -  Mise à jours des valeurs min/max lors de la création du device

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ module.exports = function(sails) {
     
     var setup = require('./lib/setup.js');
     var exec = require('./lib/exec.js');
-    
+    var send = require('./lib/send.js');
+	
     return {
         setup: setup,
-	exec: exec
+	exec: exec,
+	send: send
         };
 };
