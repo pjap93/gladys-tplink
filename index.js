34b06ed3e6dbec6dd4b28ae0648b72b93d345e7a
pjap93 <pjap9392@gmail.com>
2017-08-06 13:24:44 +0200

    Update
    - Ajout de la commande des prises HS110
    - Ajout dans identifier le type de produit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = function(sails) {
     
     var setup = require('./lib/setup.js');
     var exec = require('./lib/exec.js');
-    var send = require('./lib/send.js');
-	
+    var sendLB = require('./lib/sendLB.js');
+
     return {
         setup: setup,
 	exec: exec,
-	send: send
+	sendLB: sendLB
         };
 };
