33444f71fd70aee97de35d41312cbda9f8ce3c16
pjap93 <pjap9392@gmail.com>
2017-09-14 17:53:27 +0200

    v0.1.4
    - Ajout du suivi de consommation des prises HS110

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ module.exports = function(sails) {
     var exec = require('./lib/exec.js');
     var sendLB = require('./lib/sendLB.js');
     var sendHS = require('./lib/sendHS.js');
+    var init = require('./lib/init.js');
+    var getConsumption = require('./lib/getConsumption.js');
+
+    gladys.on('ready', function(){
+        init();
+    });
 
     return {
         setup: setup,
 	exec: exec,
 	sendLB: sendLB,
-	sendHS: sendHS
+	sendHS: sendHS,
+        getConsumption: getConsumption
         };
 };
